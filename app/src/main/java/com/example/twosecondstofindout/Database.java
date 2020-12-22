9395d959a32952480e0e0e1d4c32061d166382ad
Vorák Gergő <vorakgeri@gmail.com>
2020-12-22 21:33:26 +0100

    standard q&a template showing

--- app/src/main/java/com/example/twosecondstofindout/Database.java
+++ app/src/main/java/com/example/twosecondstofindout/Database.java
@@ -23,9 +23,9 @@ public class Database extends SQLiteOpenHelper
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE " + TABLE_KERDESEK + "(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, temakor_id INTEGER NOT NULL, kerdes VARCHAR(350) NOT NULL, valasz VARCHAR(200) NOT NULL)");
-        db.execSQL("INSERT INTO " + TABLE_KERDESEK + "(temakor_id, kerdes, valasz) VALUES (1,'asd','1')");
-        db.execSQL("INSERT INTO " + TABLE_KERDESEK + "(temakor_id, kerdes, valasz) VALUES (1,'asd','2')");
-        db.execSQL("INSERT INTO " + TABLE_KERDESEK + "(temakor_id, kerdes, valasz) VALUES (1,'asd','3')");
+        db.execSQL("INSERT INTO " + TABLE_KERDESEK + "(temakor_id, kerdes, valasz) VALUES (1,'asd?','1azért')");
+        db.execSQL("INSERT INTO " + TABLE_KERDESEK + "(temakor_id, kerdes, valasz) VALUES (1,'asd1?','2ezrt')");
+        db.execSQL("INSERT INTO " + TABLE_KERDESEK + "(temakor_id, kerdes, valasz) VALUES (1,'asd2?','3mert')");
     }
 
     @Override
@@ -36,9 +36,22 @@ public class Database extends SQLiteOpenHelper
     public Cursor selectQuestionId(int topic)
     {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor eredmeny = db.rawQuery("SELECT id FROM kerdesek WHERE temakor_id=1 LIMIT 1",null);
-        //Cursor eredmeny = db.rawQuery("SELECT id FROM " + TABLE_KERDESEK + " WHERE temakor_id = " + topic + " LIMIT 1", null);
+        Cursor eredmeny = db.rawQuery("SELECT id FROM " + TABLE_KERDESEK + " WHERE temakor_id = " + topic + " ORDER BY RANDOM() LIMIT 1", null);
         return eredmeny;
+    }
+
+    public Cursor selectQuestionText(int questionId)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor question = db.rawQuery("SELECT kerdes FROM " + TABLE_KERDESEK + " WHERE id = " + questionId, null);
+        return question;
+    }
+
+    public Cursor selectAnswerText(int questionId)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor question = db.rawQuery("SELECT valasz FROM " + TABLE_KERDESEK + " WHERE id = " + questionId, null);
+        return question;
     }
 
 }
--- app/src/main/java/com/example/twosecondstofindout/QuestionActivity.java
+++ app/src/main/java/com/example/twosecondstofindout/QuestionActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -32,10 +33,38 @@ public class QuestionActivity extends AppCompatActivity {
 
     public void kerdes(int topic)
     {
-        Question.setText(String.valueOf(topic));
-        database.selectQuestionId(topic);
-        /*StringBuffer stringBuffer=new StringBuffer();
-        String asd=eredmeny.toString();
-        Question.setText(asd);*/
+        Cursor dbQuestionId = database.selectQuestionId(topic);
+        StringBuffer stringBufferQId=new StringBuffer();
+        if (dbQuestionId != null && dbQuestionId.getCount() > 0)
+        {
+            while (dbQuestionId.moveToNext())
+            {
+                stringBufferQId.append(dbQuestionId.getString(0));
+            }
+        }
+
+        int questionId = Integer.parseInt(stringBufferQId.toString());
+        //Question.setText(String.valueOf(questionId));  string value of
+        Cursor questionText = database.selectQuestionText(questionId);
+        StringBuffer stringBufferQ=new StringBuffer();
+        if (questionText != null && questionText.getCount() > 0)
+        {
+            while (questionText.moveToNext())
+            {
+                stringBufferQ.append(questionText.getString(0));
+            }
+            Question.setText(stringBufferQ.toString());
+        }
+
+        Cursor answerText = database.selectAnswerText(questionId);
+        StringBuffer stringBufferA=new StringBuffer();
+        if (answerText != null && answerText.getCount() > 0)
+        {
+            while (answerText.moveToNext())
+            {
+                stringBufferA.append(answerText.getString(0));
+            }
+            Answer.setText(stringBufferA.toString());
+        }
     }
 }
